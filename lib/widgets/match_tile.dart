bc3ea7601de034534e7b85374c9657c22320ef11
Sobrino Julian <sobrinojulian@protonmail.com>
2024-05-26 00:41:40 -0300

    MatchTile: refactor

--- lib/widgets/match_tile.dart
+++ lib/widgets/match_tile.dart
@@ -4,68 +4,66 @@ import 'package:aquehorajuegaboca_app/models.dart';
 
 class MatchTile extends StatelessWidget {
   final Match match;
-
   const MatchTile({super.key, required this.match});
+
+  static const double _imageHeight = 48.0;
+  static const double _imageWidth = 48.0;
+  static const double _imageSpacing = 8.0;
+  static const double _cardPadding = 8.0;
+
+  Widget _buildCompetitorInfo(Competitor competitor) {
+    return Column(
+      children: [
+        Image.network(
+          competitor.logo,
+          height: _imageHeight,
+          width: _imageWidth,
+        ),
+        const SizedBox(width: _imageSpacing),
+        Text(
+          competitor.displayName,
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDateTimeInfo(DateTime date, bool tbd) {
+    final dayOfWeekFormat = DateFormat('EEE'); // Mon
+    final dateFormat = DateFormat('dd/MM'); // 24/05
+    final timeFormat = DateFormat('HH:mm'); // 14:30
+
+    return Column(
+      children: [
+        Text(dayOfWeekFormat.format(date)),
+        Text(dateFormat.format(date)),
+        tbd ? const Text('TBD') : Text(timeFormat.format(date)),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    final DateTime utcDate =
-        match.date.toUtc().add(const Duration(hours: -3)); // UTC-03:00
-
-    final DateFormat dayOfWeekFormat = DateFormat('EEE'); // Mon
-    final DateFormat dateFormat = DateFormat('dd/MM'); // 24/05
-    final DateFormat timeFormat = DateFormat('HH:mm'); // 14:30
+    final DateTime date = match.date.toLocal();
+    final Competitor homeCompetitor = match.competitors[0];
+    final Competitor awayCompetitor = match.competitors[1];
 
     return Card(
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(_cardPadding),
         child: Column(
           children: [
             Text(match.league),
             Row(
               children: [
                 Expanded(
-                  child: Column(
-                    children: [
-                      Image.network(
-                        match.competitors[0].logo,
-                        height: 48.0,
-                        width: 48.0,
-                      ),
-                      const SizedBox(width: 8.0),
-                      Text(
-                        match.competitors[0].displayName,
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
+                  child: _buildCompetitorInfo(homeCompetitor),
                 ),
                 Expanded(
-                  child: Column(
-                    children: [
-                      Text(dayOfWeekFormat.format(utcDate)),
-                      Text(dateFormat.format(utcDate)),
-                      match.tbd
-                          ? const Text('TBD')
-                          : Text(timeFormat.format(utcDate)),
-                    ],
-                  ),
+                  child: _buildDateTimeInfo(date, match.tbd),
                 ),
                 Expanded(
-                  child: Column(
-                    children: [
-                      Image.network(
-                        match.competitors[1].logo,
-                        height: 48.0,
-                        width: 48.0,
-                      ),
-                      const SizedBox(width: 8.0),
-                      Text(
-                        match.competitors[1].displayName,
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
+                  child: _buildCompetitorInfo(awayCompetitor),
                 ),
               ],
             ),
