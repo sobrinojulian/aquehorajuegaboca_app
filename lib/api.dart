46385285b5bf153f231d9a9222114f3efce311d3
Sobrino Julian <sobrinojulian@protonmail.com>
2024-05-25 21:42:06 -0300

    api.dart: fix utf rendering

--- lib/api.dart
+++ lib/api.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-
 import 'models.dart';
 
 Future<List<Match>> fetchMatches() async {
@@ -10,8 +9,10 @@ Future<List<Match>> fetchMatches() async {
   final response = await http.get(Uri.parse(matchesUrl));
 
   if (response.statusCode == 200) {
-    Iterable jsonResponse = jsonDecode(response.body);
-    return jsonResponse.map((match) => Match.fromJson(match)).toList();
+    final jsonResponse = jsonDecode(utf8.decode(response.bodyBytes));
+    return (jsonResponse as List)
+        .map((match) => Match.fromJson(match))
+        .toList();
   } else {
     throw Exception('Failed to load matches');
   }
